9212175af2f088326e9ded7dce7e5ac37f7daa17
CristhiannTello <193808135+CristhiannTello@users.noreply.github.com>
2025-01-10 07:05:45 +0000

    Pending changes exported from your codespace

---
+++ practica python 1/6 Edad y determinar si es mayor.py
@@ -0,0 +1,8 @@
+# Solicitamos la edad a la persona (algoritmo)
+edad = int(input("Ingresa tu edad: "))
+
+# Verificamos si es o no mayor de edad (algoritmo)
+if edad >= 18:
+    print("Sí eres mayor de edad.")
+else:
+    print("No eres mayor de edad.")
